b7c537d2f014502d9ebdec8ec7eb81591de1fad4
jared.lu <jared-lu@outlook.com>
2024-08-11 20:23:53 +0800

    长时间任务探查

--- internal/storage/mysql/execution_dao_test.go
+++ internal/storage/mysql/execution_dao_test.go
@@ -19,6 +19,7 @@ func TestGormExecutionDAO_InsertExecStatus(t *testing.T) {
 		id         int64
 		taskStatus task.ExecStatus
 		wantErr    error
+		wantID     int64
 	}{
 		{
 			name: "启动任务，insert一条记录",
@@ -32,6 +33,7 @@ func TestGormExecutionDAO_InsertExecStatus(t *testing.T) {
 			id:         1,
 			taskStatus: task.ExecStatusStarted,
 			wantErr:    nil,
+			wantID:     1,
 		},
 		{
 			name: "任务执行成功，更新执行记录",
@@ -39,12 +41,13 @@ func TestGormExecutionDAO_InsertExecStatus(t *testing.T) {
 				mockDB, mock, err := sqlmock.New()
 				require.NoError(t, err)
 				mock.ExpectExec("INSERT INTO `execution` .* ON DUPLICATE KEY UPDATE").
-					WillReturnResult(sqlmock.NewResult(1, 1))
+					WillReturnResult(sqlmock.NewResult(2, 1))
 				return mockDB
 			},
 			id:         1,
 			taskStatus: task.ExecStatusSuccess,
 			wantErr:    nil,
+			wantID:     2,
 		},
 	}
 	for _, tc := range testCase {
@@ -59,7 +62,48 @@ func TestGormExecutionDAO_InsertExecStatus(t *testing.T) {
 			})
 			require.NoError(t, err)
 			dao := NewGormExecutionDAO(db)
-			err = dao.InsertExecStatus(context.Background(), tc.id, tc.taskStatus)
+			id, err := dao.InsertExecStatus(context.Background(), tc.id, tc.taskStatus)
+			assert.Equal(t, tc.wantErr, err)
+			assert.Equal(t, tc.wantID, id)
+		})
+	}
+}
+
+func TestGormExecutionDAO_UpdateProgress(t *testing.T) {
+	testCases := []struct {
+		name     string
+		sqlMock  func(t *testing.T) *sql.DB
+		id       int64
+		progress int
+		wantErr  error
+	}{
+		{
+			name: "更新任务进度成功",
+			sqlMock: func(t *testing.T) *sql.DB {
+				mockDB, mock, err := sqlmock.New()
+				require.NoError(t, err)
+				mock.ExpectExec("UPDATE `execution` .*").
+					WillReturnResult(sqlmock.NewResult(1, 1))
+				return mockDB
+			},
+			id:       1,
+			progress: 10,
+			wantErr:  nil,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			sqlDB := tc.sqlMock(t)
+			db, err := gorm.Open(mysql.New(mysql.Config{
+				Conn:                      sqlDB,
+				SkipInitializeWithVersion: true,
+			}), &gorm.Config{
+				DisableAutomaticPing:   true,
+				SkipDefaultTransaction: true,
+			})
+			require.NoError(t, err)
+			dao := NewGormExecutionDAO(db)
+			err = dao.UpdateProgress(context.Background(), tc.id, uint8(tc.progress))
 			assert.Equal(t, tc.wantErr, err)
 		})
 	}
